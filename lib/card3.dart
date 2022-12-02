14787f366f8e37f1cec277a5ecb679eed6257096
thezx <thezxdev@gmail.com>
2022-12-02 13:42:06 -0700

    card3 header

--- lib/card3.dart
+++ lib/card3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
 
 class Card3 extends StatelessWidget {
   const Card3({super.key});
@@ -25,8 +26,26 @@ class Card3 extends StatelessWidget {
                 color: Colors.black.withOpacity(.6),
                 borderRadius: const BorderRadius.all(Radius.circular(15))
               ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  const SizedBox(height: 8,),
+                  Text(
+                    'Recipe Trends',
+                    style: FooderlichTheme.darkTextTheme.headline2,
+                  ),
+                  // const SizedBox(height: 30,)
+                ],
+              ),
             )
-            // TODO: Add Container, Column, Icon and Text
             // TODO: Add Center Widget with Chip Widget children
           ],
         ),
